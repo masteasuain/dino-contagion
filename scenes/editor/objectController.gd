f786d8fe8735d98d93d08ccb7ee0fb383b05e10d
jordan <gnujordan@gmail.com>
2018-07-11 23:30:10 -0300

    Level Editor progress

--- scenes/editor/objectController.gd
+++ scenes/editor/objectController.gd
@@ -3,11 +3,11 @@ extends Node
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+onready var gameController = get_tree().get_root().get_node("game")
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	print("ready")
 	pass
 
 #func _process(delta):
@@ -15,23 +15,11 @@ func _ready():
 #	# Update game logic here.
 #	pass
 
-"""
-func _on_object_toggled( button_pressed ):
-	if button_pressed:
-		dinoSelected = 0
-	else:
-		dinoSelected = -1
-"""
-func buttonToggled( button_pressed ):
-	print("hola")
-	#if button_pressed:
-		#get_tree().get_root().get_node("game").dinoSelected = 0
-		#dinoSelected = 0
-	#else:
-		#get_tree().get_root().get_node("game").dinoSelected = -1
-		
-	#print(get_tree().get_root().get_node("game").dinoSelected)
+func _on_Button_pressed():
+	gameController.dinoSelected = 0
 	#get_node("UI/SFX_click").play()
 	#get_node("UI/DinoSelector_2/post_it").pressed = false
 	#get_node("UI/DinoSelector_3/post_it").pressed = false
 	#get_node("UI/DinoSelector_4/post_it").pressed = false
+	print("selected DinoFront")
+	pass
